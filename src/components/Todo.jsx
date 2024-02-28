2bcd12e216847538f95dc2d8fc041890bcb8d624
Vshker Mayengbam <vshkermayengbam@gmil.com>
2024-02-28 19:57:21 +0530

    Adding to the list functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../context/TodoContext'
 
-const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
+
+// This component is for individual todo item
+const Todo = (props) => {
+    const [todos, setTodos] = useContext(TodoContext);
+
+    return (
+        <>
+            <p className='todo-item'>
+                <input type='checkbox' id={props.id} value={props.id} />
+                <label htmlFor={props.id}>{props.title}</label>
+                <button type="button">Delete</button>
+            </p>
+        </>
+    )
 }
 
 export default Todo
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Todo from './Todo'
+import { TodoContext } from '../context/TodoContext'
+
+// This component list the todo
 
 const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
+
+    const [todos, setTodos] = useContext(TodoContext)
+
+    return (
+        1 <= todos.length ? todos.map((item) => {
+            return(
+                <Todo key = {item.id} id = {item.id} title = {item.title} completed = {item.completed}/>
+            )
+        }) : (<h4>No todos found. Please add some...</h4>)
+    )
 }
 
 export default TodoList
