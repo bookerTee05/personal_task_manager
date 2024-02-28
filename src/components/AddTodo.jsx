4eb0a602840f954f1a826eace529ab9e9b224dd6
Vshker Mayengbam <vshkermayengbam@gmil.com>
2024-02-28 19:07:39 +0530

    function to update the input title using useState hook

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// This component is for input and add the task to the list
 
 const AddTodo = () => {
+    const [title, setTitle] = useState('')
+
   return (
-    <div>AddTodo</div>
+    <>
+        <div className='form-input-container'>
+            <input value={title} className='form-input' onChange={e => setTitle(e.target.value)} placeholder='Add to do..'/>
+            <button type='button' className='form-btn'>Add</button>
+        </div>
+    </>
   )
 }
 
