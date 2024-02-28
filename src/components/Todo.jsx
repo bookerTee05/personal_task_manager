bb6730677368b89392a75d5fa84c6bc7cd4e594c
Vshker Mayengbam <vshkermayengbam@gmil.com>
2024-02-28 20:05:40 +0530

    Checkbox function  is added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,10 +6,27 @@ import { TodoContext } from '../context/TodoContext'
 const Todo = (props) => {
     const [todos, setTodos] = useContext(TodoContext);
 
+    // make the checkbox return true
+    const completeTodo = (e) => {
+        const filterTodos = todos.map((item) => {
+            if(item.id === e.target.value) {
+                item.completed = false;
+                if (e.target.checked) {
+                    item.completed = true;
+                }
+            }
+
+            return item;
+        })
+
+        setTodos(filterTodos);
+
+    }
+
     return (
         <>
             <p className='todo-item'>
-                <input type='checkbox' id={props.id} value={props.id} />
+                <input type='checkbox' id={props.id} value={props.id} onChange={e => completeTodo(e)} />
                 <label htmlFor={props.id}>{props.title}</label>
                 <button type="button">Delete</button>
             </p>
